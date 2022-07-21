fef46cb4d8b71c147191a3da9537e3c4deb34f9a
Andrey-Abramovich <aan_84@tut.by>
2022-07-21 20:27:37 +0300

    add item_5.js

---
+++ item_5.js
@@ -0,0 +1,44 @@
+/*Переписать консольное приложение из предыдущего юнита на классы.
+
+Общие требования:
+
+    Имена классов, свойств и методов должны быть информативными;
+    Соблюдать best practices;
+    Использовать синтаксис ES6.
+*/
+
+class Device {
+    constructor(name, plugin) {
+        this.name = name
+    }
+    getOn(plugin) {
+        if (this.plugin = plugin) {
+            document.write(`Включен ${this.name} потребляемая мощность: `);
+        } else {
+            document.write(`Выключен ${this.name} потребляемая мощность: `);
+        }
+    }
+}
+
+class ElectricalAppliance extends Device {
+    constructor(power, getOn, name) {
+        super(getOn, name);
+    }
+    getPower(power, getOn, name) {
+        if(getOn) {
+            return super.getOn(true) + document.write(power / 1000 + 'Kwh' + '<br>');
+        } else {
+            return super.getOn() + document.write(power * 0);
+        }
+    }
+}
+
+const noutbook = new ElectricalAppliance();
+
+noutbook.name = 'Acer';
+noutbook.getPower(60, true);
+
+const lamp = new ElectricalAppliance();
+
+lamp.name = 'Lamp';
+lamp.getPower(10, false);
